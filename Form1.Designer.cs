58bebdcad1c267313236d3b52d648d20da90ded7
Dave Broome <dbroome@shastacollege.edu>
2018-04-25 14:27:30 -0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace RemGpsTimes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxFiles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.chkbxRemTimeHist = new System.Windows.Forms.CheckBox();
+            this.chkbxKeepFileType = new System.Windows.Forms.CheckBox();
+            this.lblAction = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGpsBabelExeLoc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstbxFiles
+            // 
+            this.lstbxFiles.AllowDrop = true;
+            this.lstbxFiles.FormattingEnabled = true;
+            this.lstbxFiles.Location = new System.Drawing.Point(56, 65);
+            this.lstbxFiles.Name = "lstbxFiles";
+            this.lstbxFiles.Size = new System.Drawing.Size(695, 277);
+            this.lstbxFiles.TabIndex = 22;
+            this.lstbxFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstbxFiles_DragDrop);
+            this.lstbxFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.lstbxFiles_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Drag/Drop Gpx or Xml Files";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(713, 49);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(47, 13);
+            this.lblStatus.TabIndex = 20;
+            this.lblStatus.Text = "lblStatus";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Maroon;
+            this.lblError.Location = new System.Drawing.Point(58, 428);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(39, 13);
+            this.lblError.TabIndex = 19;
+            this.lblError.Text = "lblError";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(697, 23);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 18;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // chkbxRemTimeHist
+            // 
+            this.chkbxRemTimeHist.AutoSize = true;
+            this.chkbxRemTimeHist.Location = new System.Drawing.Point(223, 8);
+            this.chkbxRemTimeHist.Name = "chkbxRemTimeHist";
+            this.chkbxRemTimeHist.Size = new System.Drawing.Size(125, 17);
+            this.chkbxRemTimeHist.TabIndex = 24;
+            this.chkbxRemTimeHist.Text = "Remove Timestamps";
+            this.chkbxRemTimeHist.UseVisualStyleBackColor = true;
+            this.chkbxRemTimeHist.CheckedChanged += new System.EventHandler(this.chkbxRemTimeHist_CheckedChanged);
+            // 
+            // chkbxKeepFileType
+            // 
+            this.chkbxKeepFileType.AutoSize = true;
+            this.chkbxKeepFileType.Location = new System.Drawing.Point(363, 8);
+            this.chkbxKeepFileType.Name = "chkbxKeepFileType";
+            this.chkbxKeepFileType.Size = new System.Drawing.Size(118, 17);
+            this.chkbxKeepFileType.TabIndex = 25;
+            this.chkbxKeepFileType.Text = "Keep same file type";
+            this.chkbxKeepFileType.UseVisualStyleBackColor = true;
+            this.chkbxKeepFileType.CheckedChanged += new System.EventHandler(this.chkbxKeepFileType_CheckedChanged);
+            // 
+            // lblAction
+            // 
+            this.lblAction.AutoSize = true;
+            this.lblAction.ForeColor = System.Drawing.Color.Blue;
+            this.lblAction.Location = new System.Drawing.Point(222, 33);
+            this.lblAction.Name = "lblAction";
+            this.lblAction.Size = new System.Drawing.Size(47, 13);
+            this.lblAction.TabIndex = 26;
+            this.lblAction.Text = "lblAction";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Full Path to Gpsbabel executable";
+            // 
+            // txtGpsBabelExeLoc
+            // 
+            this.txtGpsBabelExeLoc.Location = new System.Drawing.Point(56, 392);
+            this.txtGpsBabelExeLoc.Name = "txtGpsBabelExeLoc";
+            this.txtGpsBabelExeLoc.Size = new System.Drawing.Size(695, 20);
+            this.txtGpsBabelExeLoc.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 454);
+            this.Controls.Add(this.txtGpsBabelExeLoc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAction);
+            this.Controls.Add(this.chkbxKeepFileType);
+            this.Controls.Add(this.chkbxRemTimeHist);
+            this.Controls.Add(this.lstbxFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.CheckBox chkbxRemTimeHist;
+        private System.Windows.Forms.CheckBox chkbxKeepFileType;
+        private System.Windows.Forms.Label lblAction;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGpsBabelExeLoc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Diagnostics;
+
+namespace RemGpsTimes
+{
+    public partial class Form1 : Form
+    {
+        string BasePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\RemGpsTimes\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClearLabels();
+            lblAction.Text = "Convert gpx to kml and vice versa";
+            GetSetGpsbabelExePath("Get");
+            ExeFileExists();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            ClearLabels();
+            if (ExeFileExists())
+            {
+                GetSetGpsbabelExePath("Set");
+                SetStatusLabel("Working", Color.Blue);
+                ProcessFiles();
+                SetStatusLabel("Done", Color.Green);     
+            }
+
+           
+        }
+
+        private void ProcessFiles()
+        {
+            //for testing
+            //lstbxFiles.Items.Add(@"E:\Data\Other\Tracks\North\MtShasta\Test\pig-farm-trail.kml");
+
+            
+            for (int i = 0; i < lstbxFiles.Items.Count; i++)
+            {
+                string[] fileParts = GetFileParts(lstbxFiles.Items[i].ToString());
+                switch (GetAction())
+                {
+                    case "00"://Convert gpx to kml and vice versa
+                        ConvertFile(fileParts, false);
+                        break;
+                    case "01"://Will not do anything
+
+                        break;
+                    case "10"://Remove timestamps and Convert gpx to kml and vice versa
+                        ConvertFile(fileParts, true);
+                        break;
+                    case "11"://Remove timestamps, but retain file type (appends _Mod to file name)
+                        RemoveTimeHistory(fileParts, "_Mod");
+                        break;
+                }
+
+
+            }
+        }
+
+        private void ConvertFile(string[] fileParts, bool removeTimes)
+        {
+            string newExt = "kml";
+            if (fileParts[3] == "kml")
+            {
+                newExt = "gpx";
+            }
+            string newPath = fileParts[0].Replace(fileParts[1], fileParts[2] + "." + newExt);
+
+            //"E:\Program Files (x86)\GPSBabel\gpsbabel.exe" -i gpx -f "E:\Data\Other\Tracks\North\MtShasta\test\pig-farm-trail_orig - Copy.gpx" -o kml,trackdata=0 -F "E:\Data\Other\Tracks\North\MtShasta\test\cmdlinetest.kml"
+            //string cmdLine = "\"" + exePath + "\" -i " + fileParts[3] + " -f \"" + fileParts[0] + "\" -o " + newExt + " -F \"" + newPath + "\"";
+
+            string gpsbabelExe = "\"" + txtGpsBabelExeLoc.Text + "\"";
+            string args = " -i " + fileParts[3] + " -f \"" + fileParts[0] + "\" -o " + newExt + " -F \"" + newPath + "\"";
+
+            ProcessStartInfo stInfo = new ProcessStartInfo();
+            stInfo.FileName = gpsbabelExe;
+            stInfo.Arguments = args;
+
+            try
+            {
+                using (Process p = Process.Start(stInfo))
+                {
+                    p.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            if (removeTimes)
+            {
+                fileParts = GetFileParts(newPath);
+                RemoveTimeHistory(fileParts, "");
+            }
+
+        }
+
+        private string[] GetFileParts(string fullPath)
+        {
+            string[] retArr = { "fullpath", "filename", "filename w/o ext", "ext" };
+            retArr[0] = fullPath;
+            string[] bsArr = { @"\" };
+            string[] pathParts = fullPath.Split(bsArr, StringSplitOptions.None);
+            retArr[1] = pathParts[pathParts.Length - 1];//filename with ext
+            string[] fileParts = retArr[1].Split('.');
+            retArr[2] = fileParts[0].ToLower();
+            retArr[3] = fileParts[1].ToLower();
+            return retArr;
+        }
+
+        private void RemoveTimeHistory(string[] fileParts, string appendToName)
+        {
+            //E:\Data\Other\Tracks\North\MtShasta\filename.gpx
+
+            //if appendToName == "", then source file is overwritten. Should only happen when also converting to different format
+            string newPath = fileParts[0].Replace(fileParts[1], fileParts[2] + appendToName + "." + fileParts[3]);
+
+            if (fileParts[3] == "gpx")
+            {
+                RemTimeHistory(fileParts[0], newPath, "time");
+            }
+            else //kml
+            {
+                RemTimeHistory(fileParts[0], newPath, "TimeSpan");
+                RemTimeHistory(newPath, newPath, "TimeStamp");
+            }
+
+        }
+
+        private void RemTimeHistory(string source, string dest, string eleName)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(source);
+
+            XmlNodeList timeNodes = xDoc.GetElementsByTagName(eleName);
+            for (int i = timeNodes.Count - 1; i > -1; i--)
+            {
+                timeNodes[i].ParentNode.RemoveChild(timeNodes[i]);
+            }
+
+            xDoc.Save(dest);
+        }
+
+        private void SetStatusLabel(string msg, Color clr)
+        {
+            lblStatus.Text = msg;
+            lblStatus.ForeColor = clr;
+            lblStatus.Refresh();
+        }
+
+        private void ClearLabels()
+        {
+            lblError.Text = "";
+            lblStatus.Text = "";
+        }
+
+        private void lstbxFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void lstbxFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            lstbxFiles.Items.Clear(); //or add a button to the page to clear items
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+                lstbxFiles.Items.Add(file);
+        }
+
+        private void SetActionLabel()
+        {
+            switch (GetAction())
+            {
+                case "00":
+                    lblAction.Text = "Convert gpx to kml and vice versa";
+                    break;
+                case "01":
+                    lblAction.Text = "Will not do anything";
+                    break;
+                case "10":
+                    lblAction.Text = "Remove timestamps and Convert gpx to kml and vice versa";
+                    break;
+                case "11":
+                    lblAction.Text = "Remove timestamps, but retain file type (appends _Mod to file name)";
+                    break;
+            }
+
+        }
+
+        private string GetAction()
+        {
+            string retVal = chkbxRemTimeHist.Checked ? "1" : "0";
+            retVal += chkbxKeepFileType.Checked ? "1" : "0";
+            return retVal;
+        }
+
+        private void chkbxRemTimeHist_CheckedChanged(object sender, EventArgs e)
+        {
+            SetActionLabel();
+        }
+
+        private void chkbxKeepFileType_CheckedChanged(object sender, EventArgs e)
+        {
+            SetActionLabel();
+        }
+
+        private void GetSetGpsbabelExePath(string getSet)
+        {
+            try
+            {
+                if (!Directory.Exists(BasePath))
+                {
+                    Directory.CreateDirectory(BasePath);
+                }
+                string fullPath = BasePath + "GpsbabelExePath.txt";
+                if (!File.Exists(fullPath))
+                {
+                    CreateGpsbabelPathFile(fullPath, @"c:\Program Files (x86)\GPSBabel\gpsbabel.exe"); //set default
+                }
+
+                if (getSet == "Get")
+                {
+                    StreamReader sr = new StreamReader(fullPath);
+                    txtGpsBabelExeLoc.Text = sr.ReadLine();
+                    sr.Close();
+                }
+                else
+                {
+                    CreateGpsbabelPathFile(fullPath, txtGpsBabelExeLoc.Text);
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+
+        private void CreateGpsbabelPathFile(string filePath, string exeLocation)
+        {
+            StreamWriter sw = new StreamWriter(filePath);
+            sw.Write(exeLocation);
+            sw.Close();
+        }
+
+        private bool ExeFileExists()
+        {
+            if (!File.Exists(txtGpsBabelExeLoc.Text))
+            {
+                lblError.Text = "Above file doesn't exist. Enter full path to 'gpsbabel.exe' in textbox ";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+
+
+    }//
+}
